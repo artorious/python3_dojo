7e7d8fc589f3e7525df303e4fce55670461930cd
Arthur Ngondo <ngondoarthur@gmail.com>
2017-12-15 15:58:50 +0300

    Add custom module - get_positive_number_from_user

---
+++ custom_modules/get_positive_number_from_user.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+'''Prompt user for a positve number ( integer or floating-point number).
+Returns the value
+'''
+
+def get_positive_num():
+    '''Prompt user for a positive number (integer or floating-point number)'''
+    # init
+    valid_value = False
+
+    while not valid_value:
+        try:
+            value = float(input('Enter a positive number: '))  # Enter Max value
+            if value > 0:
+                valid_value = True # Terminate loop after getting a valid value
+            else:
+                print('Please enter a positive number')
+                continue # Try entering value again
+
+        except ValueError:
+            print('Expected a number')
+
+    return value
+
+if __name__ == '__main__':
+    get_positive_num()
+
+
+
+
