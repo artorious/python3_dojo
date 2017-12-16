9c322ee6a22324eecf49bd4f729043b85b59020d
Arthur Ngondo <ngondoarthur@gmail.com>
2017-12-16 10:20:38 +0300

    Add die_roll simulation. - 3 rolls -

---
+++ die_roll.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+'''Random Numbers
+A program that simulates the rolling of a die.
+'''
+from random import randrange
+
+for i in range(0, 3): # 3 Rolls
+    value = randrange(1, 7) # Six die-faces
+
+    print('+-------+')
+    if value == 1:
+        print('|       |')
+        print('|   *   |')
+        print('|       |')
+    elif value == 2:
+        print('| *     |')
+        print('|       |')
+        print('|     * |')
+    elif value == 3:
+        print('|     * |')
+        print('|   *   |')
+        print('| *     |')
+    elif value == 4:
+        print('| *   * |')
+        print('|       |')
+        print('| *   * |')
+    elif value == 5:
+        print('| *   * |')
+        print('|   *   |')
+        print('| *   * |')
+    elif value == 6:
+        print('| * * * |')
+        print('|       |')
+        print('| * * * |')
+    else:
+        print('*** Error: Illegal die value ***')
+    print('+-------+')
+
