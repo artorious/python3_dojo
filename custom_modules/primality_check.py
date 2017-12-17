d7dffb05f8f5e945c08010c775b64e547f4bc9ef
Arthur Ngondo <ngondoarthur@gmail.com>
2017-12-17 10:47:08 +0300

    Add primality_check module containing is_prime()

---
+++ custom_modules/primality_check.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+"""Test for Primality.
+Tests for primality each integer from 2 up to a value provided by the user.  
+If an integer is prime, it prints it; otherwise, the number is not printed.
+Note: 
+    A prime number is an integer greater than 1 whose only factors (divisors)
+    are 1 and the number itself
+"""
+
+from math import sqrt
+from get_positive_number_from_user import get_positive_num
+
+##################################################################################################
+
+def is_prime(the_integer):
+    """
+    Determines the primality of a given value <the_integer>, an integer.
+    Returns True if <the_integer>, otherwise, returns False
+    """
+
+    # Try potential factors from 2 (smallest prime) to the square root of <the_integer> (inclusive)
+    # Use of squareroot to increase program efficiency (Instead of Trying values from 2  to <the_integer> (inclusive))
+    root = round(sqrt(the_integer)) + 1 
+
+    for trial_factor in range(2, root):
+        if the_integer % trial_factor == 0:     # Is it a factor
+            return False                        # Found a Factor
+    return True                                 # No factors found
+
+######################################################################################################
+
+def main():
+    """Check for Primality"""
+    print(__doc__) # Program Greeting
+
+    # Init
+    max_value = int(get_positive_num())
+
+    for value in range(2, max_value + 1):   # Step thro all potential primes
+        if is_prime(value):                 # Check if prime
+            print(value, end=" ")           # Display primes found
+    print()                                 # Move cursor to the  next line
+
+if __name__ == '__main__':
+    main()
