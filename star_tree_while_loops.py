2fd8319a48b9bf4600e3d75362d7e8d2348ca097
Arthur Ngondo <ngondoarthur@gmail.com>
2017-12-18 10:13:29 +0300

    Reorganize star_tree_while_loops.py  program into functions

--- star_tree_while_loops.py
+++ star_tree_while_loops.py
@@ -13,31 +13,37 @@ Height = 6
                ***********
 '''
 from custom_modules.get_positive_number_from_user import get_positive_num
-
-print(__doc__) # Program Greeting
-# init
-row= 0  # Draw one row for every unit of height
-height = int(get_positive_num()) # Get tree height (positive integer) from user
-
-###---- Printing Tree ----###
-
-while row < height: # Draws one row of the tree each time its's body executes
+def draw_tree(height):
+    """ Draws a tree of a given <height>
+    <height> being an integer
+    """
+    row = 0 # Init first row, from the top, to draw
+    while row < height: # Draws one row of the tree each time its's body executes
     
-    # Print Leading spaces; 
-        # as row gets bigger, no. of leading spaces gets smaller
-    count = 0
-    while count < height - row:
-        print(end=' ')
-        count += 1
-    
-    # Print out stars, twice the current row plus one:
-    #   1. No. of stars on the left side of tree = current row value
-    #   2. Exacltly one star in the center of the tree
-    #   3. No. of stars on the right side of tree = current row value
+        # Print Leading spaces; 
+            # as row gets bigger, no. of leading spaces gets smaller
+        count = 0
+        while count < height - row:
+            print(end=' ')
+            count += 1
+        
+        # Print out stars, twice the current row plus one:
+        #   1. No. of stars on the left side of tree = current row value
+        #   2. Exacltly one star in the center of the tree
+        #   3. No. of stars on the right side of tree = current row value
 
-    count = 0
-    while count < ((2 * row) + 1): 
-        print(end='*')
-        count += 1
-    print() # Move to the next line
-    row += 1 # Next row
+        count = 0
+        while count < ((2 * row) + 1): 
+            print(end='*')
+            count += 1
+        print() # Move to the next line
+        row += 1 # Next row
+
+
+    
+
+if __name__ == '__main__':
+    print(__doc__) # Program Greeting
+    draw_tree(int(get_positive_num()))  # Draw a tree with height from user
+
+
