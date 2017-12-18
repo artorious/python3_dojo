489aa6ca18ef64b069b6bf74a3d4811d1d6813b5
Arthur Ngondo <ngondoarthur@gmail.com>
2017-12-18 10:08:29 +0300

    Reorganize star_tree_for_loops.py  program into functions

--- star_tree_for_loops.py
+++ star_tree_for_loops.py
@@ -14,26 +14,38 @@ Height = 6
 '''
 from custom_modules.get_positive_number_from_user import get_positive_num
 
-print(__doc__) # Program Greeting
-# init
-height = int(get_positive_num()) # Get tree height (positive integer) from user
 
+def draw_tree(height):
+    """ Draws a tree of a given <height>
+    <height> being an integer
+    """
+    for row in range(height): # Draws one row for every unit of height
+    
+        # Print Leading spaces; 
+            # as row gets bigger, no. of leading spaces gets smaller
+        for count in range(height - row):
+            print(end=' ')
+        
+        # Print out stars, twice the current row plus one:
+        #   1. No. of stars on the left side of tree = current row value
+        #   2. Exacltly one star in the center of the tree
+        #   3. No. of stars on the right side of tree = current row value
+
+        for count in range((2 * row) + 1): 
+            print(end='*')
+
+        print() # Move to the next line
+        
+
+
+
+    
+
+if __name__ == '__main__':
+    print(__doc__) # Program Greeting
+    draw_tree(int(get_positive_num())) # Draw a tree with height from user 
+
+
+ 
 ###---- Printing Tree ----###
 
-for row in range(height): # Draws one row for every unit of height
-    
-    # Print Leading spaces; 
-        # as row gets bigger, no. of leading spaces gets smaller
-    for count in range(height - row):
-        print(end=' ')
-    
-    # Print out stars, twice the current row plus one:
-    #   1. No. of stars on the left side of tree = current row value
-    #   2. Exacltly one star in the center of the tree
-    #   3. No. of stars on the right side of tree = current row value
-
-    for count in range((2 * row) + 1): 
-        print(end='*')
-
-    print() # Move to the next line
-    
