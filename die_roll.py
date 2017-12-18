4aaaaf1c8ee7e8dfdfc5eba87efb4027932f013e
Arthur Ngondo <ngondoarthur@gmail.com>
2017-12-18 09:59:35 +0300

    Reorganize die_roll.py program into functions

--- die_roll.py
+++ die_roll.py
@@ -1,38 +1,59 @@
 #!/usr/bin/env python3
-'''Random Numbers
+'''Die Rolling Simulator
+
 A program that simulates the rolling of a die.
 '''
 from random import randrange
+from custom_modules.get_positive_number_from_user import get_positive_num
 
-for i in range(0, 3): # 3 Rolls
-    value = randrange(1, 7) # Six die-faces
-
+def show_die(spots):
+    """
+    Draws a picture of a die with the number of <spots> indicated.
+    <spots> is the number of spots on the top face. 
+    """
     print('+-------+')
-    if value == 1:
+    if spots == 1:
         print('|       |')
         print('|   *   |')
         print('|       |')
-    elif value == 2:
+    elif spots == 2:
         print('| *     |')
         print('|       |')
         print('|     * |')
-    elif value == 3:
+    elif spots == 3:
         print('|     * |')
         print('|   *   |')
         print('| *     |')
-    elif value == 4:
+    elif spots == 4:
         print('| *   * |')
         print('|       |')
         print('| *   * |')
-    elif value == 5:
+    elif spots == 5:
         print('| *   * |')
         print('|   *   |')
         print('| *   * |')
-    elif value == 6:
+    elif spots == 6:
         print('| * * * |')
         print('|       |')
         print('| * * * |')
     else:
-        print('*** Error: Illegal die value ***')
+        print('*** Error: Illegal die spots ***')
     print('+-------+')
+
+def roll():
+    """ Returns a pseudorandom number in the range 1...6, inclusive """
+    return randrange(1, 7) # Six die-faces
+
+def simulate_die(num_of_rolls):
+    """ Simulates the roll of a die <num_of_rolls> 
+    <num_of_rolls> is a positive integer indicating the number of rolls
+    """
+    for a_roll in range(1, num_of_rolls + 1):
+        show_die(roll())
+
+
+if __name__ == '__main__':
+    simulate_die(int(get_positive_num()))
+
+    
 
