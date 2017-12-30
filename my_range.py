59af2757878f10c4fcdb02f435b8d2d2c8d4a886
Arthur Ngondo <ngondoarthur@gmail.com>
2017-12-30 21:06:38 +0300

    Simulate  the behaviour of the built-in range expression

---
+++ my_range.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+""" A play at generators
+Simulate  the behaviour of the built-in range expression
+NOTE:
+    A generator is an object that generates a sequence of values.
+    Code that uses a generator may obtaian one value at a time without the 
+    possibility of revisiting earlier values. (consume a generator's product)
+"""
+
+def range_custom(arg1, arg2=None, step=1):
+    """(int, int, int) - generator
+    Simulate  the behaviour of the built-in range expression
+    """
+    
+    if arg2 != None: # Do we have atleast two args
+        begin = arg1
+        end = arg2
+    else: # When it's one arg, default start at zero
+        begin = 0
+        end = arg1
+            
+    i = begin
+    while i != end:
+        yield i
+        i += step
+
+if __name__ == '__main__':
+    for num in range_custom(1, 11, 2):
+        print(num, end=' ')
