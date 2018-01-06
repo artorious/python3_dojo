6ac208f9880c2ef81dbb18dde663c55725d92bc8
Arthur Ngondo <ngondoarthur@gmail.com>
2018-01-06 16:52:47 +0300

    Create initial layout - positions of the starting horses on the turtle graphics screen

--- horse_race_simulation.py
+++ horse_race_simulation.py
@@ -13,15 +13,56 @@
 # Algorithmic approach
     # Advance each horse at a random distance at fixed intervals until one
     # reaches a certain point on the turtle screen (finish line)
-
-### OVERALL STEPS
-    # Initialize Turtle Graphics
-    # Execute race horse simulation
 #-----------------------------------------------------------------------------
 
-# TODO: Create an Initial Turtle Screen Layout
-# Layout positions of the starting horses on the turtle graphics screen
+# TODO: Execute race horse simulation
+# TODO: Include specif shapes and images for the simulation
+import turtle
+
+def new_horse():
+    """(None) -> turtle
+    Create a new horse
+    Returns a turtle object
+    """
+    horse = turtle.Turtle()
+    return horse
+
+def generate_horses(num_horses):
+    """(int) -> list
+    Returns a list of turtle objects assigned to variable horses
+    """
+    horses = []
+    for a_horse in range(0, num_horses):
+        horse = new_horse()
+        horses.append(horse)
+    return horses
+
+def place_horses(horses, loc, separation):
+    """ (list, tuple, int) -> turtle
+    Positions each turtle object in <horses>, 
+    <loc> being coordinates for the first turtle, 
+    and the amount of <separation> between each
+    horse
+    """
+    for a_horse in range(0, len(horses)):
+        horses[a_horse].hideturtle()
+        horses[a_horse].penup()
+        horses[a_horse].setposition(loc[0], loc[1] + a_horse * separation)
+        horses[a_horse].setheading(180)
+        horses[a_horse].showturtle()
+
+def main():
+    """ Horse Race Simulation """
+    num_horses = 10 # Init number of horses
+    turtle.setup(750, 800)  # Set window size
+    window = turtle.Screen() # Init turtle window
+    window.title('Horse Race Simulation Program') # Set window title bar
+    start_loc = (240, -200) # Init Location of the first horse
+    track_separation = 60   # Init amount of separation between each horse
+    horses = generate_horses(num_horses) # Generate & Init horses
+    place_horses(horses, start_loc, track_separation)   # Place horses at starting line
+    turtle.exitonclick() # Terminate program when close window
 
 
-
-
+if __name__ == '__main__':
+    main()
