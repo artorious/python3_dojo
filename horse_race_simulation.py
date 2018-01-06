fb6add6a3ddc916644623850dc2abcaf35918443
Arthur Ngondo <ngondoarthur@gmail.com>
2018-01-06 15:58:56 +0300

    Describe problem - Race horse simulation program

---
+++ horse_race_simulation.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+""" Horse Race Simulation Program """
+### THE PROBLEM
+# Create a visualisation  of a horse race in which horses are moved ahead at
+# random distance at fixed intervals until there is a winner
+### PROGRAM DESIGN
+# Meeting the program requirements
+    # Create an appropriate simulation of a 10 horse race
+# Data description
+    # Current location of each of the 10 horses in a given race
+    # Maintain 10 turtle objects (attributes include shape & coordinates)
+    # Use/create suitable horse images
+# Algorithmic approach
+    # Advance each horse at a random distance at fixed intervals until one
+    # reaches a certain point on the turtle screen (finish line)
+
+### OVERALL STEPS
+    # Initialize Turtle Graphics
+    # Execute race horse simulation
+#-----------------------------------------------------------------------------
+
+# TODO: Create an Initial Turtle Screen Layout
+# Layout positions of the starting horses on the turtle graphics screen
+
+
+
+
