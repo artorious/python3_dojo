9b915ceeced25d5c4458a415aee69b9c961d8ea8
Arthur Ngondo <ngondoarthur@gmail.com>
2018-01-08 05:53:07 +0300

    Add stubbed functions for a simple programmer-defined stack module - Implemented as a list

---
+++ stack.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+""" A simple programmer-defined stack module - Implemented as a list
+
+Module contains a set of functions that implement stack behavior
+
+Stacks are used to temporarily store and retrieve data.
+The last item placed on the stack is the first to be retrieved. 
+LIFO - Last In First Out. 
+A stack can be viewed as a list that can be accessed only at one end.
+"""
+
+def get_stack():
+    """ Creates and returns an empty stack """
+    return
+
+def is_empty(the_stack):
+    """ Returns True if stack empty, otherwise returns False. """
+    return
+
+def top(the_stack):
+    """ Returns value of the top item o stack, if stack not empty. 
+    Otherwise returns None """
+
+    return None
+
+def push(the_stack, item):
+    """ Pushes item on the top of stack """
+    pass
+
+def pop(the_stack):
+    """ Returns top of stack if stack not empty. Otherwise, returns None """
+    return
+
+def main():
+    """ Display and push values 1 through 4 on the stack 
+    
+    Displays the numbers popped off the stack, retrieved in the reverse order
+    that they were pushed
+    """
+    pass
+
+if __name__ == '__main__':
+    main()
