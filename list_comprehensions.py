6c2587072f29c02e8c4734c3986abc9d5ff34bb0
Arthur Ngondo <ngondoarthur@gmail.com>
2018-01-14 18:55:36 +0300

    Build a list of factor pairs for a positive integer - A Play on Python List Comprehensions

--- list_comprehensions.py
+++ list_comprehensions.py
@@ -2,6 +2,7 @@
 """ A Play on Python List Comprehensions """
 
 from custom_modules.get_positive_number_from_user import get_positive_num
+import math
 
 def factors_singles(num):
     """ (int) -> list
@@ -11,10 +12,31 @@ def factors_singles(num):
     """
     return [x for x in range(1, num + 1) if num % x == 0]
 
+def factors_pairs(num):
+    """ (int) -> list
+    Builds a list of factor pairs for a positive integer <num>. 
+    for example, tuple (2, 15) is a factor pair for the number 30 
+    because 2 x 15 is 30.
+
+    Returns the list
+    """
+    return [(x, num//x) for x in range(1, num + 1) if num % x == 0]
+
+def unique_factor_pairs(num):
+    """ (int) -> list
+    Builds a list of unique factor pairs for a positive integer <num>. 
+    for example, tuple (2, 15) is a factor pair for the number 30 
+    because 2 x 15 is 30.
+
+    Returns the list
+    """
+    pass
+
 def main():
     """ A Play on Python List Comprehension """
     n = int(get_positive_num())
     print('Factors of {0}: {1}'.format(n, factors_singles(n)))
+    print('Factor pairs of {0}: {1}'.format(n, factors_pairs(n)))
 
 if __name__ == '__main__':
     main()
