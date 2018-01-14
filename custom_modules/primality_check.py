87a4de8b6a107d6ed1803e58437b2899f66dd1af
Arthur Ngondo <ngondoarthur@gmail.com>
2018-01-14 21:33:51 +0300

    Add primes_list_comprehension() - Build and return a list that contains all the prime numbers

--- custom_modules/primality_check.py
+++ custom_modules/primality_check.py
@@ -38,23 +38,43 @@ def prime_sequence(start, stop):
         if is_prime(value) == True:
             yield value
 
+def primes_list_comprehension(num):
+    """ (int) - list
 
+    For all num ≥ 2, the following list comprehension creates a list of 
+    all the factors of n, not including 1 and n itself. 
+
+    Builds and returns  a list that contains all the prime numbers less
+    than <num>. 
+    """
+    return [p for p in range(2, num) if not [x for x in range(2, p) if p % x == 0]] 
     
+
+# TODO:
+def prime_seq_sieve(max_potential_prime):
+    """ (int) -> list
+
+    Prime Generation with a List - implements the Sieve of Eratosthenes 
+    in a Python function.
+
+    Uses an algorithm developed by the Greek mathematician Eratosthenes who 
+    lived from 274 B.C. to 195 B.C. Called the Sieve of Eratosthenes, 
+    the principle behind the algorithm is simple: 
+
+    Make a list of all the integers two and larger. 
+    Two is a prime number, but any multiple of two cannot be a prime number 
+    (since a multiple of two has two as a factor). Go through the rest of the 
+    list and mark out all multiples of two (4, 6, 8, ...). 
+    Move to the next number in the list (in this case, three). 
+    If it is not marked out, it must be prime, 
+    so go through the rest of the list and mark out all multiples of that 
+    number (6, 9, 12, ...). Continue this process until you have listed 
+    all the primes you want.
+    """
+    pass
     
 
 if __name__ == '__main__':
     from get_positive_number_from_user import get_positive_num
-
-    print("Check for Primality") # Program Greeting
-
-    # Init
-    max_value = int(get_positive_num())
-
-    cntr = 0
-
-    for value in range(2, max_value + 1):   # Step thro all potential primes
-        if is_prime(value):                 # Check if prime
-            cntr += value
-            print(value, end=" ")           # Display primes found
-    print()                                 # Move cursor to the  next line
-    print('Total =', cntr)
+    num = int(get_positive_num())
+    print(primes_list_comprehension(num))
