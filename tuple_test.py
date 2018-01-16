6700f08214908c6ad1e06dc2c606d7b1a5c32df0
Arthur Ngondo <ngondoarthur@gmail.com>
2018-01-16 16:39:48 +0300

    Construct a sequence of tuples from tw - list comprehensiono lists

--- tuple_test.py
+++ tuple_test.py
@@ -49,6 +49,7 @@ print()
 # Construct a sequence of tuples from two lists
 lst1 = [1, 2, 3, 4, 5, 6, 7, 8]
 lst2 = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h']
-
-for t in zip(lst1, lst2):
-    print(t)
+lst3 = []
+[lst3.append(t) for t in zip(lst1, lst2)]
+print(lst3)
+print()
