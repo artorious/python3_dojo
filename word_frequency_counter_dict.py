50cc442bc5057ff80694daa56f924c9ba2a634c7
Arthur Ngondo <ngondoarthur@gmail.com>
2018-01-16 22:31:52 +0300

    Modify occurrence count of each word in a text file to use comprehensions

--- word_frequency_counter_dict.py
+++ word_frequency_counter_dict.py
@@ -19,25 +19,16 @@ def main():
         print('Usage: python3 word_frequency_counter_dict.py <filename>')
         print('where <filename> is the name of a text file.')
     
-    else:                                           # Filename provided
-        file_name = sys.argv[1]
-        counters = {}                               # Init counting dict
+    else:   # Filename provided
+        file_name = sys.argv[1]                              
         
         with open(file_name, 'r') as txt_file_obj:  # Open file for reading
             txt_file_content = txt_file_obj.read()  # Read content of whole file
-            words = txt_file_content.split()        # Make list of individual words
-
-        for word in words:
-            word = word.upper()   # Make All-caps
-            
-            if word not in counters:
-                counters[word] = 1 # 1st occurence of a word
-            else:
-                counters[word] += 1 # Incr existing count
-
-        # Report
-        for word, count in counters.items():
-            print('{0} : {1}'.format(word, count))
-
+            words = [word.upper() for word in txt_file_content.split()]  # Make list of individual words (All-caps)
+            words_count = {word:words.count(word) for word in set(words)}    # Init counting dict
+            # Report
+            for word, count in words_count.items():
+                print('{0} : {1}'.format(word, count))
+        
 if __name__ == '__main__':
     main()
