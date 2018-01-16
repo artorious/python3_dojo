924e5e2ffaff278f660b6a2cae7ba91d230ba902
Arthur Ngondo <ngondoarthur@gmail.com>
2018-01-16 17:35:29 +0300

    Make a new list from two existing lists

--- zip_seq.py
+++ zip_seq.py
@@ -1,12 +1,7 @@
 #!/usr/bin/env python3
-""" A play on Python lists and tuples 
- 
- Construct a sequence of tuples with their first elements derived from a list
- and their second elements obtained from a generator.
- """
- # NOTE: Zip a list with a generator sequence that is shorter than the list
+""" A play on Python lists and tuples """
 
-def sequence_generator(num):
+def perfect_sq_seq_gen(num):
      """ (int) -> generator
      
      Generates the first <num> perfect squares,
@@ -15,5 +10,32 @@ def sequence_generator(num):
      for i in range(num):
         yield i ** 2
 
-[print(p, end=' ') for p in zip(list(range(10)), sequence_generator(5))]   
+def main():    
+    """ A play on Python lists and tuples """
+    
+    print('''
+    Construct a sequence of tuples with their first elements derived from a 
+    list and their second elements obtained from a generator.
+    ''')
+    print('Tuple sequence:\v', end='')
+     # NOTE: generator sequence is shorter than the list
+    [print(_, end=' ') for _ in zip(list(range(10)), perfect_sq_seq_gen(5))]
+    print()
+    
+    print('''
+    Make a new list from two existing lists. Where:
+    
+    The 1st element in the new list is the sum of the first elements from 
+    the two original lists. Similarly, the 2nd element in the new list is the
+    sum of the second elements in the two original lists, and so forth. 
+    ''')
+    lst1, lst2  = list(range(1, 6)), list(range(10, 15))
+    print('Working lists:\v{0} and {1}'.format(lst1, lst2))
+    print('List of sums:\v{0}'.format([x+y for (x, y) in zip(lst1, lst2)]))
+    # for (x, y) in zip(lst1, lst2):
+    #     print(x + y, end=' ')
+
+if __name__ == '__main__':
+    main()
+   
 
