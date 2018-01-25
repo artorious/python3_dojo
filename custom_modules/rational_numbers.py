308403fa388a17bfb5f239c2587b5aa266378551
Arthur Ngondo <ngondoarthur@gmail.com>
2018-01-25 11:19:24 +0300

    Add Relational operators to the fraction class

--- custom_modules/rational_numbers.py
+++ custom_modules/rational_numbers.py
@@ -27,7 +27,7 @@ class Rational(object):
         if denominator != 0:
             self.__denominator = denominator
         else:
-            raise ValueError('DivisionByZeroError: zero cant be denominator')
+            raise ValueError('DivisionByZeroError: 0 can\'t be a denominator')
 
     def get_numerator(self):
         """ (Rational) -> int
@@ -63,13 +63,19 @@ class Rational(object):
             raise ValueError("DivisionByZeroError: zero cant be denominator")
 
     def reduce(self):
-        """ Simplify Fraction """
+        """ (Rational) -> Rational
+        
+        Simplify Fraction 
+        """
         gcd = self.__calc_gcd()
         self.__numerator = self.__numerator // gcd
         self.__denominator = self.__denominator // gcd
 
     def __calc_gcd(self):
-        """ Calculates GCD """
+        """ (Rational) -> int
+        
+        Calculates GCD 
+        """
         a = max(self.__numerator, self.__denominator)
         b = min(self.__numerator, self.__denominator)
 
@@ -95,7 +101,8 @@ class Rational(object):
         return Rational(-self.__numerator, self.__denominator)
 
     def __add__(self, r_fraction):
-        """ 
+        """ (Rational, Rational) -> Rational
+
         Returns a new reduced fraction equal to <self> + <r_fraction>
         """
         numer = self.__numerator * r_fraction.get_denominator() +\
@@ -109,7 +116,8 @@ class Rational(object):
         return result_addition
     
     def __sub__(self, r_fraction):
-        """
+        """ (Rational, Rational) -> Rational
+
         Returns a new reduced fracion equal to <self> - <r_fraction>
         """
 
@@ -128,29 +136,187 @@ class Rational(object):
         result_multiply.reduce()
 
         return result_multiply
+    
+    def copy(self):
+        """ (Rational, Rational) -> Rational
 
-# TODO: Add Relational operators to the fraction class    
+        Make a copy of the fraction 
+        """
+        new_frac = Rational(self.__numerator, self.__denominator)
+        return new_frac
+
+    def __adjust(self, factor):
+        """ (Rational, int) -> Rational
+        
+        Adjust fraction by <factor>
+        """
+        self.set_numerator(self.get_numerator() * factor)
+        self.set_denominator(self.get_denominator() * factor)
+
+    def __eq__(self, r_fraction):
+        """ (Rational, Rational) -> bool
+        
+        Returns True if <self> arithmetically equal to <r_fraction>
+        otherwise, returns False
+        """
+        temp_frac1 = self.copy()
+        temp_frac2 = r_fraction.copy()
+        temp_frac1.reduce()
+        temp_frac2.reduce()
+
+        if temp_frac1.get_numerator() == temp_frac2.get_numerator() and \
+           temp_frac1.get_denominator() == temp_frac2.get_denominator():
+            return True
+        else:
+            return False
+    
+    def __neq__(self, r_fraction):
+        """(Rational, Rational) -> bool
+        
+        Returns True if <self> is NOT arithmetically equal to <r_fraction>
+        otherwise, returns False
+        """
+        return not self.__eq__(r_fraction)
+
+    def __lt__(self, r_fraction):
+        """(Rational, Rational) -> bool
+        
+        Returns True if <self> is less than <r_fraction>
+        """
+        if self.get_denominator() == r_fraction.get_denominator:
+            return self.get_numerator() < r_fraction.get_numerator()
+        else:
+            temp_frac1 = self.copy()
+            temp_frac2 = r_fraction.copy()
+
+            saved_denom = temp_frac1.get_denominator()
+            temp_frac1.__adjust(temp_frac2.get_denominator())
+            temp_frac2.__adjust(saved_denom)
+
+            return temp_frac1.get_numerator() < temp_frac2.get_numerator()
+
+    def __le__(self, r_fraction):
+        """(Rational, Rational) -> bool
+        
+        Returns True if <self> is less than or equal to <r_fraction>
+        """
+        if self == r_fraction or self < r_fraction:
+            return True
+        else:
+            return False
+
+    def __gt__(self, r_fraction):
+        """(Rational, Rational) -> bool
+        
+        Returns True if <self> is greater than <r_fraction>
+        """
+        if not(self <= r_fraction):
+            return True
+        else:
+            return False
+
+    def __ge__(self, r_fraction):
+        """(Rational, Rational) -> bool
+        
+        Returns True if <self> is greater than or equal to <r_fraction>
+        """
+        if not(self < r_fraction):
+            return True
+        else:
+            return False
+
+
 def main():
     """ Client code that uses Rational objects """
     fract1 = Rational(1, 2)
     fract2 = Rational(1, 4)
-    print('Fraction 1: {0}'.format(fract1))
-    print('Fraction 2: {0}'.format(fract2))
-    print('{0} * {1} = {2}'.format(fract1, fract2, Rational.__multiply__(fract1, fract2)))
-    print('{0} + {1} = {2}'.format(fract1, fract2, Rational.__add__(fract1, fract2)))
-    print('{0} - {1} = {2}'.format(fract1, fract2, Rational.__sub__(fract1, fract2)))
-    print('Negate({0}) = {1}'.format(fract1, Rational.__neg__(fract1)))
+    fract3 = Rational(2, 4)
+
+    print('Arithmetic operations on Fractions; {0}, {1} and {2}'.format(
+        fract1, fract2, fract3))
+    print()
+    print('{0} * {1} = {2}'.format(
+        fract1, fract2, fract1.__multiply__(fract2)))
+    print('{0} + {1} = {2}'.format(
+        fract1, fract2, fract1.__add__(fract2)))
+    print('{0} - {1} = {2}'.format(
+        fract1, fract2, fract1.__sub__(fract2)))
+
+    print('Negate({0}) = {1}'.format(fract1, fract1.__neg__()))
+    print('Negate({0}) = {1}'.format(fract2, fract2.__neg__()))
+    
+    print()
+    print('Relational operations on Fractions; {0}, {1} and {2}'.format(
+            fract1, fract2, fract3))
+    print()
+    print('{0} is equal to {1} : {2}'.format(
+        fract1, fract2, fract1.__eq__(fract2)))
+    print('{0} is equal to {1} : {2}'.format(
+        fract3, fract1, fract3.__eq__(fract1)))
+    print('{0} is equal to {1} : {2}'.format(
+        fract2, fract3, fract2.__eq__(fract3)))
+
+    print('{0} is NOT equal to {1} : {2}'.format(
+        fract1, fract2, fract1.__neq__(fract2)))
+    print('{0} is NOT equal to {1} : {2}'.format(
+        fract3, fract1, fract3.__neq__(fract1)))
+
+    print('{1} is less than {0} : {2}'.format(
+        fract1, fract2, fract2.__lt__(fract1)))
+    print('{0} is less than {1} : {2}'.format(
+        fract3, fract1, fract3.__lt__(fract1)))
+
+    print('{1} is less or equal to {0} : {2}'.format(
+        fract1, fract2, fract2.__lt__(fract1)))
+
+    print('{1} is greater than {0} : {2}'.format(
+        fract1, fract2, fract2.__gt__(fract1)))
+    print('{0} is greater than {1} : {2}'.format(
+        fract3, fract1, fract3.__gt__(fract1)))
+
+    print('{1} is greater than or equal to {0} : {2}'.format(
+        fract1, fract2, fract2.__ge__(fract1)))
+    print('{0} is greater than or equal to {1} : {2}'.format(
+        fract3, fract1, fract3.__ge__(fract1)))
+    print()
         
     fract1.set_numerator(3)
     fract1.set_denominator(4)
     fract2.set_numerator(2)
     fract2.set_denominator(3)
-    print('Fraction 1: {0}'.format(fract1))
-    print('Fraction 2: {0}'.format(fract2))
-    print('{0} * {1} = {2}'.format(fract1, fract2, Rational.__multiply__(fract1, fract2)))
-    print('{0} + {1} = {2}'.format(fract1, fract2, Rational.__add__(fract1, fract2)))
-    print('{0} - {1} = {2}'.format(fract1, fract2, Rational.__sub__(fract1, fract2)))
+
+    print('Arithmetic operations on Fractions; {0} and {1}'.format(
+        fract1, fract2))
+    print()
+    print('{0} * {1} = {2}'.format(
+        fract1, fract2, Rational.__multiply__(fract1, fract2)))
+    print('{0} + {1} = {2}'.format(
+        fract1, fract2, Rational.__add__(fract1, fract2)))
+    print('{0} - {1} = {2}'.format(
+        fract1, fract2, Rational.__sub__(fract1, fract2)))
+
     print('Negate({0}) = {1}'.format(fract1, Rational.__neg__(fract1)))
+    print()
+    
+    print('Relational operations on Fractions; {0} and {1}'.format(
+        fract1, fract2))
+    print()
+    print('{0} is equal to {1} : {2}'.format(
+        fract1, fract2, Rational.__eq__(fract1, fract2)))
+
+    print('{0} is NOT equal to {1} : {2}'.format(
+        fract1, fract2, Rational.__neq__(fract1, fract2)))
+
+    print('{0} is less than {1} : {2}'.format(
+        fract1, fract2, Rational.__lt__(fract1, fract2)))
+
+    print('{0} is less or equal to {1} : {2}'.format(
+        fract1, fract2, Rational.__le__(fract1, fract2)))
+
+    print('{0} is greater than {1} : {2}'.format(
+        fract1, fract2, Rational.__gt__(fract1, fract2)))
+    print('{0} is greater than or equal to {1} : {2}'.format(
+        fract1, fract2, Rational.__ge__(fract1, fract2)))
 
     # fract_bad = Rational(4, 0)
 
